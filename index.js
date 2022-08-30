20997580977a9819a7303968bfe7fe7cfe34f670
yugmani <yugmani@yahoo.com>
2022-08-30 10:20:07 -0700

    get the largest value from an array

--- index.js
+++ index.js
@@ -24,3 +24,26 @@ const getSmallestValue = (arr) =>
   arr.reduce((smallest, num) => Math.min(smallest, num));
 
 console.log(getSmallestValue(array1)); //3
+
+// 2. Get the Largest Element of an Array
+// ***************************************
+
+const getLargest = (arr) => {
+  let largest = Number.NEGATIVE_INFINITY;
+
+  for (const num of arr) {
+    if (num > largest) {
+      largest = num;
+    }
+  }
+
+  return largest;
+};
+
+console.log(getLargest(array1)); // 17
+
+//Alternative
+const getLargestValue = (arr) =>
+  arr.reduce((largest, num) => Math.max(largest, num));
+
+console.log(getLargestValue(array1)); //17
